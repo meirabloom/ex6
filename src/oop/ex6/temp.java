7fe42731ff0b3c12a291a47eb33744d67d837249
Meira <meirabloom@gmail.com>
2018-06-15 19:00:10 +0300

    hi

--- src/oop/ex6/temp.java
+++ src/oop/ex6/temp.java
@@ -4,4 +4,12 @@ public class temp {
     public static void main(String[] args) {
         System.out.println("abigail");
     }
+
+    //TODO : a boolean expression representing the validity of the code. initialized (where?) as true, once
+    //TODO                an exception is thrown/ an error is found, is switched to false and the run ends
+
+
+
+    //TODO : where to close the buffered reader?
+    //TODO: do we need to reset at the end of the program run?
 }
