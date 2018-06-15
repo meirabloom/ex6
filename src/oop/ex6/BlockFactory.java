5eab8ced6deb887b545facea5b0d7f54b629b789
Meira <meirabloom@gmail.com>
2018-06-15 18:55:58 +0300

    hi

--- src/oop/ex6/BlockFactory.java
+++ src/oop/ex6/BlockFactory.java
@@ -2,6 +2,7 @@ package oop.ex6;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -9,13 +10,38 @@ import java.util.LinkedList;
  */
 public class BlockFactory {
 
+    /** the array containing the blocks */
+    ArrayList<Block> blockList = new ArrayList<>();
+
     //block types
     static final String METHOD = "method";
     static final String WHILE = "while";
     static final String IF = "if";
 
+    /**
+     * iterates over the list of lines and creates the block objects, adding each block to the list of blocks
+     */
+    public void parseBlocks() { //TODO
 
-    public BlockFactory(LinkedList<String> codeLines){
+    }
 
+    /**
+     *
+     * @param type - the type of the block: method, if or while
+     * @param blockLines - the lines making up the block //TODO not sure if this is what we want to do
+     * @param parentBlock - the scope in which this block is nested
+     * @return - the new block object according to its type
+     */
+    public Block createBlock(String type, LinkedList blockLines , Block parentBlock){ //TODO exceptions
+        switch (type){
+            case METHOD:
+                return new MethodBlock(); //TODO what paramaters do these objects receive?
+            case WHILE:
+                return new WhileBlock();
+            case IF:
+                return new IfBlock();
+            default:
+                return null;
+        }
     }
 }
