2728cf17d68dafcc4df077b7474666267f4852e4
Meira <meirabloom@gmail.com>
2018-06-15 12:59:08 +0300

    hi

--- src/oop/ex6/BlockFactory.java
+++ src/oop/ex6/BlockFactory.java
@@ -2,8 +2,20 @@ package oop.ex6;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.LinkedList;
 
+/**
+ * iterates over the string lines of code and parses them into block objects according to their type
+ */
 public class BlockFactory {
 
-    public BlockFactory(BufferedReader reader, )
+    //block types
+    static final String METHOD = "method";
+    static final String WHILE = "while";
+    static final String IF = "if";
+
+
+    public BlockFactory(LinkedList<String> codeLines){
+
+    }
 }
